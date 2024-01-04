2b62cdba052b68433bd37a73c01c93fb1bec0248
ning0426 <15996204@qq.com>
2024-01-04 17:41:40 +0800

    添加测试

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,15 @@
 import Parse, { AREA, Utils } from "./parse/index.js";
 
 test("广东省东莞寮步镇香市路香缤城市花园", () => {
-  const result = Parse.parse("广东省东莞寮步镇香市路香缤城市花园");
+  const result = Parse.parse("广东省东莞寮步镇香市路香缤城市花园", true);
   console.log(result);
   expect(result[0]["code"]).toEqual("441928");
 });
 
 test("大头儿子 15988888888  重庆市云阳县双林镇桐林小学", () => {
   const result = Parse.parse(
-    "大头儿子 15988888888  重庆市云阳县双林镇桐林小学"
+    "大头儿子 15988888888  重庆市云阳县双林镇桐林小学",
+    true
   );
   console.log(result);
   expect(result[0]["code"]).toEqual("500235");
